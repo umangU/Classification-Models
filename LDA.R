6a34034de5b92ac80a12fc40f720d67ca87f0bc0
umangU <72771903+umangU@users.noreply.github.com>
2021-04-11 15:23:13 +1000

    Code Updated

--- LDA.R
+++ LDA.R
@@ -11,13 +11,13 @@ dim(Smarket.2005)
 Direction.2005=Direction[!train]
 # Fitting the LDA model using the predictors with least p-values
 lda.fit = lda(Direction~Lag1+Lag2, data=Smarket, susbet=train)
-lda.fit
 # Plot of linear discriminants for each of the training observations
 plot(lda.fit)
 # Predicting on the test dataset
 lda.pred=predict(lda.fit, Smarket.2005)
 lda.class = lda.pred$class
 table(lda.class, Direction.2005)
+# Caluclate the mean error
 mean(lda.class==Direction.2005)
 # Applying 50% threshold to the posterior probability to re-create the predictions
 sum(lda.pred$posterior[,1]>=0.5)
